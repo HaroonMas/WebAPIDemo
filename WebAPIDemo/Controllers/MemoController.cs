a2818142016fe8bdf1f8e235fae99c96ae13315a
haroon masjedi <haroon_masjide@hotmail.com>
2019-10-10 00:05:58 -0400

    Add project files.

---
+++ WebAPIDemo/Controllers/MemoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIDemo.Models;
+
+namespace WebAPIDemo.Controllers
+{
+    public class MemoController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<Memo> Get()
+        {
+            //return new string[] { "value1", "value2" };
+            return new Memo[] { new Memo() { id = "1", Text = "Text1", priority = 1 },
+                               new Memo() {id ="2", Text="Text2", priority = 2 }};
+        }
+
+        // GET api/<controller>/5
+        public Memo Get(int id)
+        {
+            return new Memo() { id = "1", Text = "Text1", priority = 4 };
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebAPIDemo/Models/Memo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIDemo.Models
+{
+    public class Memo
+    {
+        public string id { get; set; }
+        public string Text { get; set; }
+        public decimal priority { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="oneLine">TRUE WILL PRINT ON ONE LINE ONLY.</param>
+        /// <returns>THE FORMATTED STRING.</returns>
+        public string PrintMemo(bool oneLine)
+        {
+            //Not using oneLine for now.
+            return String.Format("Id: {0}, Text{1}, priority{2}", this.id, this.Text, this.priority);
+        }
+    }
+}
